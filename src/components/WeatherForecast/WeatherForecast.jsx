c5a20a43a2c661d8e09048d5d3200c6309b1f518
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-25 15:07:28 -0600

    added weather icon and weather data components

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import "./WeatherForecast.css";
+import WeatherIcon from "../WeatherIcon/WeatherIcon.jsx";
+import WeatherData from "../WeatherData/WeatherData.jsx";
+
+const WeatherForecast = ({ forecast }) => {
+    return (
+        <div className="weather">
+            <h2>{forecast.day}</h2>
+            <WeatherIcon img={forecast.img} alt={forecast.alt} />
+            <WeatherData conditions={forecast.conditions} time={forecast.time} />
+           
+        </div>
+    )
+} 
+
+export default WeatherForecast;
